ca664371eb5d57b5067843e3019c127bbb209ad5
confident-token-1019 <theoonetj@gmail.com>
2019-12-18 13:37:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -40,6 +40,11 @@ else
 end
 end
 
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+ 
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
 
 
 def find_the_cheese(array)   # code an argument here
