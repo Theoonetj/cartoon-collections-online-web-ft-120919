db28a276e88af5d705bf4ecc515998e42518d103
confident-token-1019 <theoonetj@gmail.com>
2019-12-18 13:35:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,8 @@ returnArray
 end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+summon_captain_planet(planeteer_calls)
+
 
 
 def long_planeteer_calls(array) # code an argument here
